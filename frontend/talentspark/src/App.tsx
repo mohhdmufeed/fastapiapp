edb10e853fe38528ec80dfba8b932452b0afafde
Mufeed <mohhdmufeed@gmail.com>
2026-06-29 16:26:20 +0530

    frontendd

---
+++ frontend/talentspark/src/App.tsx
@@ -0,0 +1,14 @@
+import Welcome from "./components/welcome";
+import NavBar from "./components/NavBar";
+
+
+
+function App(){
+  return (
+    <>
+    <NavBar/>
+    <Welcome/>
+    </>
+  )
+}
+export default App
---
+++ frontend/talentspark/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer>
+            <p> All rights reserved by Talentspark</p>
+        </footer>
+    )
+}
+
+export default Footer
+
+
